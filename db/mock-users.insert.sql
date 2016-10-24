4f80a935264b42f45f0ac2d6c770dfc9b7e7bab9
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-24 16:47:09 -0700

    Add user role to mock users
    - 1 organizer
    - the rest are in the 'member' role

--- db/mock-users.insert.sql
+++ db/mock-users.insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `role`, `phone_number`, `invite_status`)
 VALUES
-	(1, 'Casey', 'Garcia', 'caseyggarcia@gmail.com', NULL, NULL, NULL),
-	(2, 'Cindy', 'Manit', 'cindymanit@gmail.com', NULL, NULL, NULL),
-	(3, 'Ben', 'Lee', 'brl221@yahoo.com', NULL, NULL, NULL),
-	(4, 'Mahaliyah', 'Grace', 'effervescentearth@gmail.com', NULL, NULL, NULL),
-	(5, 'Amad', 'Khan', 'amadxk@gmail.com', NULL, NULL, NULL),
-	(6, 'Benji', 'Antalek', 'benjaminolki@gmail.com', NULL, NULL, NULL),
-	(7, 'Meijiao', 'Png', 'meijiao@gmail.com', NULL, NULL, NULL),
-	(8, 'Regina', 'Fletcher', 'the18thcenturyfox@yahoo.com', NULL, NULL, NULL),
-	(9, 'Rob', 'Kaluhiokalani', 'rkaluhiokalani@gmail.com', NULL, NULL, NULL),
-	(10, 'Kvin', 'Lertpiriyapong', 'klertpiriyapong@westernu.edu', NULL, NULL, NULL);
+	(1, 'Casey', 'Garcia', 'caseyggarcia@gmail.com', 2, NULL, NULL),
+	(2, 'Cindy', 'Manit', 'cindymanit@gmail.com', 2, NULL, NULL),
+	(3, 'Ben', 'Lee', 'brl221@yahoo.com', 2, NULL, NULL),
+	(4, 'Mahaliyah', 'Grace', 'effervescentearth@gmail.com', 2, NULL, NULL),
+	(5, 'Amad', 'Khan', 'amadxk@gmail.com', 2, NULL, NULL),
+	(6, 'Benji', 'Antalek', 'benjaminolki@gmail.com', 1, NULL, NULL),
+	(7, 'Meijiao', 'Png', 'meijiao@gmail.com', 2, NULL, NULL),
+	(8, 'Regina', 'Fletcher', 'the18thcenturyfox@yahoo.com', 2, NULL, NULL),
+	(9, 'Rob', 'Kaluhiokalani', 'rkaluhiokalani@gmail.com', 2, NULL, NULL),
+	(10, 'Kvin', 'Lertpiriyapong', 'klertpiriyapong@westernu.edu', 2, NULL, NULL);
