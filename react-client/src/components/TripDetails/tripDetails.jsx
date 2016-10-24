1c263c1edff8172ef44c69db61f680f1eaf4045e
Oolkii <benjaminolki@gmail.com>
2016-10-24 12:15:59 -0700

    add text boxes for description and address field one

--- react-client/src/components/TripDetails/tripDetails.jsx
+++ react-client/src/components/TripDetails/tripDetails.jsx
@@ -4,11 +4,14 @@ class TripDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    events: []
+    events: [],
+    value: ''
     };
     //anytime you bind a function to this - do it here in this way -
     // bind only runs once on intializations, and not each render
     // this.intitialize = this.intitialize.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   // intitialize() {
   //   // return new Promise(function(resolve, reject) {
@@ -17,6 +20,12 @@ class TripDetails extends React.Component {
   //     // });
   //   // });
   // }
+  handleChange() {
+
+  }
+  handleSubmit() {
+
+  }
   componentWillMount() {
     // this.intitialize().then(function(data) {
     //   that.setState({somethingWithData});
@@ -24,15 +33,58 @@ class TripDetails extends React.Component {
   }
   render() {
     return (
-      <FirstChild/>
+      <div>
+        <DescriptionWhen 
+          value={this.state.value}
+          handleChange = {this.handleChange}
+          handleSubmit = {this.handleSubmit}
+        /> 
+        <WhereLocation 
+          value={this.state.value}
+          handleChange = {this.handleChange}
+          handleSubmit = {this.handleSubmit}
+        />
+      </div>
     );
   }
 }
 
-var FirstChild = (props) => {
+var DescriptionWhen = (props) => {
+
   return (
-    <div>Hello Again!!</div>
+    <div className="desc-container">
+      <h4>description</h4>
+      <div>
+        <input
+          className = "tripDescription"
+          type="text"
+          placeholder="Hello!"
+          value={props.value}
+          onChange={props.handleChange}
+        />
+      </div>
+      <button onClick={props.handleSubmit}>Submit</button>
+    </div>
   )
 }
+
+var WhereLocation = (props) => {
+  return (
+    <div className="where-container">
+        <h4>Where Would You Like To Go?</h4>
+        <div>
+          <input
+            className = "addressField"
+            type="text"
+            placeholder="AddressField1"
+            value={props.value}
+            onChange={props.handleChange}
+          />
+        </div>
+      <button onClick={props.handleSubmit}>Submit</button>
+      </div>
+  )
+}
+
 
 export default TripDetails
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,37 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './components/App.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-    events: []
-    };
-    //anytime you bind a function to this - do it here in this way -
-    // bind only runs once on intializations, and not each render
-    this.intitialize = this.intitialize.bind(this);
-  }
-  intitialize() {
-    // return new Promise(function(resolve, reject) {
-      // $.get(someUrl, function(data) {
-      //   resolve(data);
-      // });
-    // });
-  }
-  componentWillMount() {
-    // this.intitialize().then(function(data) {
-    //   that.setState({somethingWithData});
-    // });
-  }
-  render() {
-    // if () {
-    //   return (
-    //     <div>Pending Results</div>
-    //   );
-    // }
-    return (
-      <div>Hello World!</div>
-    );
-  }
-}
 ReactDOM.render(<App />, document.getElementById('app'))
