a3766276d17ab47bf638f3e521790c85d6f577ed
meijiao <meijiao@gmail.com>
2016-10-24 13:03:54 -0700

    Add termsfolder

---
+++ react-client/src/components/Terms/termsMaker.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class Terms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    events: [],
+    value: ''
+    };
+    //anytime you bind a function to this - do it here in this way -
+    // bind only runs once on intializations, and not each render
+    // this.intitialize = this.intitialize.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // intitialize() {
+  //   // return new Promise(function(resolve, reject) {
+  //     // $.get(someUrl, function(data) {
+  //     //   resolve(data);
+  //     // });
+  //   // });
+  // }
+  handleChange() {
+
+  }
+  handleSubmit() {
+
+  }
+  componentWillMount() {
+    // this.intitialize().then(function(data) {
+    //   that.setState({somethingWithData});
+    // });
+  }
+  render() {
+    return (
+      <div>
+        <Form /> 
+      </div>
+    );
+  }
+}
+
+
+var Form = (props) => {
+  return (
+    <h1> Terms Page in Progress </h1>
+  )
+}
+
+
+export default Terms
---
+++ react-client/src/components/termsPage/termsPageUser.jsx
@@ -0,0 +1,6 @@
+function Terms(props) {
+  return (
+    <div className="Term"> <h2> hi </h2>
+    </div>
+  );
+}
