2140bf038b71658790d46c29756c3858964197db
Oolkii <benjaminolki@gmail.com>
2016-10-24 22:18:03 -0700

    populates buttons library

--- react-client/src/components/gearList/tabView.jsx
+++ react-client/src/components/gearList/tabView.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import GearListMaker from './gearListMaker.jsx'
 import GearTabMaker from './gearTabMaker.jsx'
 
-const TabView = ({ appendInput, handleChange, handleTabSubmit, tabs, inputs, gearCategory }) => {
-  return (
-    <div className="GearListInTab">
-      <GearTabMaker 
-        tabs={ tabs }
-        handleChange={ handleChange }
-        handleTabSubmit={ handleTabSubmit }
-        gearCategory={ gearCategory }
-      />
-      <GearListMaker 
-        appendInput={ appendInput } 
-        handleChange={ handleChange } 
-        inputs={ inputs }
-      />
-    </div>
-  )
-}
+const TabView = ({ appendInput, handleChange, handleTabSubmit, tabs, inputs, gearCategory }) => (
+  <div className="GearListInTab">
+    <GearTabMaker 
+      tabs={ tabs }
+      handleChange={ handleChange }
+      handleTabSubmit={ handleTabSubmit }
+      gearCategory={ gearCategory }
+    />
+
+    <GearListMaker 
+      appendInput={ appendInput } 
+      handleChange={ handleChange } 
+      inputs={ inputs }
+    />
+  </div>
+)
+
 
 export default TabView;
