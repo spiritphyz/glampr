c48ef00b13eb1265f7ba3bb1011a4ac034ba2b22
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-24 17:06:51 -0700

    Add a few phone numbers to mock users

--- db/mock-users.insert.sql
+++ db/mock-users.insert.sql
@@ -4,9 +4,10 @@ VALUES
 	(2, 'Cindy', 'Manit', 'cindymanit@gmail.com', 2, NULL, NULL),
 	(3, 'Ben', 'Lee', 'brl221@yahoo.com', 2, NULL, NULL),
 	(4, 'Mahaliyah', 'Grace', 'effervescentearth@gmail.com', 2, NULL, NULL),
-	(5, 'Amad', 'Khan', 'amadxk@gmail.com', 2, NULL, NULL),
-	(6, 'Benji', 'Antalek', 'benjaminolki@gmail.com', 1, NULL, NULL),
-	(7, 'Meijiao', 'Png', 'meijiao@gmail.com', 2, NULL, NULL),
+	(5, 'Amad', 'Khan', 'amadxk@gmail.com', 2, '415-708-7270', NULL),
+	(6, 'Benji', 'Antalek', 'benjaminolki@gmail.com', 1, '607-765-0585', NULL),
+	(7, 'Meijiao', 'Png', 'meijiao@gmail.com', 2, '650-395-7407', NULL),
 	(8, 'Regina', 'Fletcher', 'the18thcenturyfox@yahoo.com', 2, NULL, NULL),
 	(9, 'Rob', 'Kaluhiokalani', 'rkaluhiokalani@gmail.com', 2, NULL, NULL),
-	(10, 'Kvin', 'Lertpiriyapong', 'klertpiriyapong@westernu.edu', 2, NULL, NULL);
+	(10, 'Kvin', 'Lertpiriyapong', 'klertpiriyapong@westernu.edu', 2, NULL, NULL),
+	(11, 'Tony', 'Le', 'heytonyle@gmail.com', 2, '415-265-6196', NULL);
