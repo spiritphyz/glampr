7f852fbf8e407141ae3f0d27330fdeb143b67cc7
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-26 22:10:34 -0700

    Insert tables into MySQL, untested
    - Not sure if there are subtle datatype errors or foreign key errors
    that would cause initial table creation to fail if SQL is directly
    pasted into MySQL console.
    
    - This SQL was generated by WWW SQL Designer and I've often had errors
    with the auto-generated SQL.

--- db/mysql-init-tables.sql
+++ db/mysql-init-tables.sql
@@ -17,66 +17,28 @@ CREATE TABLE `users` (
   `first_name` CHAR NULL DEFAULT NULL,
   `last_name` CHAR NULL DEFAULT NULL,
   `email` CHAR NULL DEFAULT NULL,
-  `role` SMALLINT NULL DEFAULT NULL,
+  `password` CHAR NULL DEFAULT NULL,
+  `role` CHAR NULL DEFAULT NULL,
   `phone_number` VARCHAR NULL DEFAULT NULL,
-  `invite_status` SMALLINT NULL DEFAULT NULL,
+  `tags` CHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'equips'
+-- Table 'borrow'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `equips`;
+DROP TABLE IF EXISTS `borrow`;
 
-CREATE TABLE `equips` (
+CREATE TABLE `borrow` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `user_id` INT NULL DEFAULT NULL,
-  `trip_id` INTEGER NULL DEFAULT NULL,
   `name` CHAR NULL DEFAULT NULL,
   `description` CHAR NULL DEFAULT NULL,
   `sizing` CHAR NULL DEFAULT NULL,
   `quantity` INTEGER NULL DEFAULT NULL,
   `borrow_slot` SMALLINT NULL DEFAULT NULL,
-  `tags` CHAR NULL DEFAULT NULL,
-  `status` SMALLINT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'myclothes'
--- 
--- ---
-
-DROP TABLE IF EXISTS `myclothes`;
-
-CREATE TABLE `myclothes` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `user_id` INTEGER NULL DEFAULT NULL,
-  `name` CHAR NULL DEFAULT NULL,
-  `description` CHAR NULL DEFAULT NULL,
-  `sizing` CHAR NULL DEFAULT NULL,
-  `quantity` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'food'
--- 
--- ---
-
-DROP TABLE IF EXISTS `food`;
-
-CREATE TABLE `food` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `user_id` INTEGER NULL DEFAULT NULL,
-  `name` CHAR NULL DEFAULT NULL,
-  `description` CHAR NULL DEFAULT NULL,
-  `quantity` INTEGER NULL DEFAULT NULL,
-  `amount` INTEGER NULL DEFAULT NULL,
-  `amount_units` CHAR NULL DEFAULT NULL,
-  `share_status` SMALLINT NULL DEFAULT NULL,
+  `share_status` CHAR NULL DEFAULT NULL,
   `tags` CHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
@@ -91,14 +53,43 @@ DROP TABLE IF EXISTS `trips`;
 CREATE TABLE `trips` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `title` CHAR NULL DEFAULT NULL,
-  `description` CHAR NULL DEFAULT NULL,
+  `description` MEDIUMTEXT NULL DEFAULT NULL,
   `start_date` DATETIME NULL DEFAULT NULL,
   `end_date` DATETIME NULL DEFAULT NULL,
   `address` CHAR NULL DEFAULT NULL,
-  `google_maps_url` CHAR NULL DEFAULT NULL,
+  `google_maps_url` MEDIUMTEXT NULL DEFAULT NULL,
   `cost_per_member` DECIMAL NULL DEFAULT NULL,
   `cost_deadline` DATETIME NULL DEFAULT NULL,
   `tags` CHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'borrow_users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `borrow_users`;
+
+CREATE TABLE `borrow_users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `borrow_id` INTEGER NULL DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'terms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `terms`;
+
+CREATE TABLE `terms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `trip_id` INTEGER NULL DEFAULT NULL,
+  `description` MEDIUMTEXT NULL DEFAULT NULL,
+  `category` CHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -113,59 +104,40 @@ CREATE TABLE `trips_users` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `trip_id` INTEGER NULL DEFAULT NULL,
   `user_id` INTEGER NULL DEFAULT NULL,
+  `invite_status` CHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'equips_users'
+-- Table 'required'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `equips_users`;
+DROP TABLE IF EXISTS `required`;
 
-CREATE TABLE `equips_users` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `user_id` INTEGER NULL DEFAULT NULL,
-  `equip_id` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'images'
--- 
--- ---
-
-DROP TABLE IF EXISTS `images`;
-
-CREATE TABLE `images` (
+CREATE TABLE `required` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `trip_id` INTEGER NULL DEFAULT NULL,
-  `user_id` INTEGER NULL DEFAULT NULL,
-  `equip_id` INTEGER NULL DEFAULT NULL,
-  `url_small_size` CHAR NULL DEFAULT NULL,
-  `url_big_size` CHAR NULL DEFAULT NULL,
+  `name` CHAR NULL DEFAULT NULL,
   `description` CHAR NULL DEFAULT NULL,
-  `width` INTEGER NULL DEFAULT NULL,
-  `height` INTEGER NULL DEFAULT NULL,
-  `type` INTEGER NULL DEFAULT NULL,
+  `sizing` CHAR NULL DEFAULT NULL,
+  `required` INTEGER NULL DEFAULT NULL,
   `tags` CHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- ---
--- Table 'tripterms'
+-- Table 'required_users'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `tripterms`;
+DROP TABLE IF EXISTS `required_users`;
 
-CREATE TABLE `tripterms` (
+CREATE TABLE `required_users` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `trip_id` INTEGER NULL DEFAULT NULL,
+  `required_id` INTEGER NULL DEFAULT NULL,
   `user_id` INTEGER NULL DEFAULT NULL,
-  `description` CHAR NULL DEFAULT NULL,
-  `decision` INTEGER NULL DEFAULT NULL,
-  `type` INTEGER NULL DEFAULT NULL,
+  `status` CHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -173,53 +145,45 @@ CREATE TABLE `tripterms` (
 -- Foreign Keys 
 -- ---
 
-ALTER TABLE `equips` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
-ALTER TABLE `equips` ADD FOREIGN KEY (trip_id) REFERENCES `trips` (`id`);
-ALTER TABLE `myclothes` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
-ALTER TABLE `food` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `borrow_users` ADD FOREIGN KEY (borrow_id) REFERENCES `borrow` (`id`);
+ALTER TABLE `borrow_users` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `terms` ADD FOREIGN KEY (trip_id) REFERENCES `trips` (`id`);
 ALTER TABLE `trips_users` ADD FOREIGN KEY (trip_id) REFERENCES `trips` (`id`);
 ALTER TABLE `trips_users` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
-ALTER TABLE `equips_users` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
-ALTER TABLE `equips_users` ADD FOREIGN KEY (equip_id) REFERENCES `equips` (`id`);
-ALTER TABLE `images` ADD FOREIGN KEY (trip_id) REFERENCES `trips` (`id`);
-ALTER TABLE `images` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
-ALTER TABLE `images` ADD FOREIGN KEY (equip_id) REFERENCES `equips` (`id`);
-ALTER TABLE `tripterms` ADD FOREIGN KEY (trip_id) REFERENCES `trips` (`id`);
-ALTER TABLE `tripterms` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `required` ADD FOREIGN KEY (trip_id) REFERENCES `trips` (`id`);
+ALTER TABLE `required_users` ADD FOREIGN KEY (required_id) REFERENCES `required` (`id`);
+ALTER TABLE `required_users` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
 
 -- ---
 -- Table Properties
 -- ---
 
 -- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `equips` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `myclothes` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `food` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `borrow` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `trips` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `borrow_users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `terms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `trips_users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `equips_users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `images` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `tripterms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `required` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `required_users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `users` (`id`,`first_name`,`last_name`,`email`,`role`,`phone_number`,`invite_status`) VALUES
--- ('','','','','','','');
--- INSERT INTO `equips` (`id`,`user_id`,`trip_id`,`name`,`description`,`sizing`,`quantity`,`borrow_slot`,`tags`,`status`) VALUES
--- ('','','','','','','','','','');
--- INSERT INTO `myclothes` (`id`,`user_id`,`name`,`description`,`sizing`,`quantity`) VALUES
--- ('','','','','','');
--- INSERT INTO `food` (`id`,`user_id`,`name`,`description`,`quantity`,`amount`,`amount_units`,`share_status`,`tags`) VALUES
--- ('','','','','','','','','');
+-- INSERT INTO `users` (`id`,`first_name`,`last_name`,`email`,`password`,`role`,`phone_number`,`tags`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `borrow` (`id`,`name`,`description`,`sizing`,`quantity`,`borrow_slot`,`share_status`,`tags`) VALUES
+-- ('','','','','','','','');
 -- INSERT INTO `trips` (`id`,`title`,`description`,`start_date`,`end_date`,`address`,`google_maps_url`,`cost_per_member`,`cost_deadline`,`tags`) VALUES
 -- ('','','','','','','','','','');
--- INSERT INTO `trips_users` (`id`,`trip_id`,`user_id`) VALUES
+-- INSERT INTO `borrow_users` (`id`,`borrow_id`,`user_id`) VALUES
 -- ('','','');
--- INSERT INTO `equips_users` (`id`,`user_id`,`equip_id`) VALUES
--- ('','','');
--- INSERT INTO `images` (`id`,`trip_id`,`user_id`,`equip_id`,`url_small_size`,`url_big_size`,`description`,`width`,`height`,`type`,`tags`) VALUES
--- ('','','','','','','','','','','');
--- INSERT INTO `tripterms` (`id`,`trip_id`,`user_id`,`description`,`decision`,`type`) VALUES
--- ('','','','','','');
+-- INSERT INTO `terms` (`id`,`trip_id`,`description`,`category`) VALUES
+-- ('','','','');
+-- INSERT INTO `trips_users` (`id`,`trip_id`,`user_id`,`invite_status`) VALUES
+-- ('','','','');
+-- INSERT INTO `required` (`id`,`trip_id`,`name`,`description`,`sizing`,`required`,`tags`) VALUES
+-- ('','','','','','','');
+-- INSERT INTO `required_users` (`id`,`required_id`,`user_id`,`status`) VALUES
+-- ('','','','');
