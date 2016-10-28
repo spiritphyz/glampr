90ae415c8ad9f69c1e34732ddc73c6aaf46abe27
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-27 20:45:11 -0700

    Use morgan middlware for logging

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var morgan = require('morgan'); // http request logger
 var app = express();
-var tripInfoRouter = require('./routers/tripInfo.js')
-var userRouter = require('./routers/users.js')
+var tripInfoRouter = require('./routers/tripInfo.js');
+var userRouter = require('./routers/users.js');
 
 var bodyParser = require('body-parser');
 
+app.use(morgan('dev')); // http request logger
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../react-client/dist'));
 
