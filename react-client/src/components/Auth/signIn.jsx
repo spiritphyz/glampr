f7688d75c2b58a76831b5eaeef002d6b29250b2c
spiritphyz <tonyle@ocf.berkeley.edu>
2016-10-31 11:07:59 -0700

    Stylize the SignUp view

--- react-client/src/components/Auth/signIn.jsx
+++ react-client/src/components/Auth/signIn.jsx
@@ -67,26 +67,32 @@ const SignIn = (props) => {
   }
 
   return (
-    <div className ="container center">
-      <h1> Sign In </h1>
+    <div className ="">
+      <img className="img-thumbnail float-xs-right" width="700" src="images/great-outdoors/starry-tent.jpg" />
+      <h1> Sign In</h1>
       <div>
+        <p>
+        User<br />
         <input 
           data-type="username"
           type="text" 
-          placeholder="Username(email)"
+          placeholder="email"
           onChange= {handleChange}
         />
+        </p>
       </div>
       <div>
+        <p>
+        Password<br />
         <input 
           data-type="password" 
           type="password" 
-          placeholder="password"
+          placeholder=""
           onChange={handleChange}
         />
+        </p>
       </div>
-      <button id="submit" onClick={signIn}> Submit </button>
-
+      <button className="btn btn-warning btn-lg btn-sm" id="submit" onClick={signIn}> Submit </button>
     </div>
   );
 }
